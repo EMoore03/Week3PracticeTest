19c4534a9ef59b654f16cdbab7024a216271280c
Ethan Moore <Moore-E51@ulster.ac.uk>
2024-10-15 11:20:55 +0100

    Add project files.

---
+++ CW1/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW1
+{
+    internal class Person
+    {
+        // Private fields to store the persons first name, last name and age
+        private string firstName; 
+        private string lastName;
+        private int age;
+
+        //Public property to access and modify the first name
+        public string FirstName
+        {
+            get { return firstName; } // get the current value of firstName
+            set { firstName = value; } // Set a new value to firstName
+
+        }
+
+       //Public property to access and modify the last name
+        public string LastName
+        {
+            get { return lastName; } // get the current value of lastNamw
+            set { lastName = value; } // set a new value to lastName
+        }
+
+        //Public property to access and modify age
+        public int Age
+        {
+            get { return age; } // get the current value of age
+            set { age = value; } // set a new value to age
+        }
+
+        //*** CONSTRUCTOR to initialise the fields when a new object is created 
+        public Person(String FirstName, String LastName, int Age)
+        {
+            this.FirstName = FirstName; // Assign the constructor parameter to the FirstName property
+            this.LastName = LastName;   // Assign the constructor parameter to the LastName property
+            this.Age = Age; // Assign the constructor parameter to the Age property
+        }
+
+        // Method to print the persons details to the console
+        public void PrintName()
+        {
+            Console.WriteLine("First name : " + firstName); // print the first name
+            Console.WriteLine("Last name : " + lastName); // Print the last name
+            Console.WriteLine("Age : " + age); // Print the age
+            Console.WriteLine(); // Add an empty line for spacing
+        }
+
+        public static Person CalculateOldest(Person p1, Person p2) // New method to calculte the oldest person
+        {
+            if (p1.age > p2.age)
+            {
+                return p1; // if p1 is older, return p1
+            }
+            else
+            {
+                return p2; // if p2 is older return p2
+            }
+
+        }
+    }
+}
---
+++ CW1/Program.cs
@@ -0,0 +1,74 @@
+﻿using CW1;
+using System;
+
+namespace YourNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person person = new Person("Ethan", "Moore", 23); // Create a new 'Person' object with first name, last name and age
+            person.PrintName(); // Call the 'PrintName' method to display persons details
+
+            Person person1 = new Person("Ellie", "Dixon", 20);
+            person1.PrintName(); // Display Ellies details, REMEMBER person1 = person1.PrintName!!
+
+            Person person2 = new Person("Davy", "Scott", 46);
+            person2.PrintName(); // Print Davys detaild, REMEMBER person2 = person2.PrintName!!!
+
+            Person oldestPerson = Person.CalculateOldest(person1, person2); // Call the CalculateOldest method to find the oldest person
+
+            Console.WriteLine("The oldest person is: "); // display all information about the oldest person
+            oldestPerson.PrintName(); // This will print the details
+            
+            Task1();
+            Task2();
+            Task3();
+        }
+
+        static void Task1()
+        {
+            for (int num = 1; num <= 10; num++) // Looping from 1-10
+            {
+                Console.WriteLine(num); // Printing the results
+            }
+            Console.WriteLine(); //Adding a break between task 1 & 2
+        }
+
+        static void Task2()
+        {
+            for (int num = 10; num >=1 ; num--) // Looping from 10-1
+            {
+                Console.WriteLine(num); // Printing the results
+            }
+        }
+
+        static void Task3()
+        {
+            int count = 1; // initalise count
+            int sum = 0; //initalise to store
+            while (count != 0) // when count is not equal to 0
+            {
+                Console.WriteLine("Please enter a integer, Enter 0 to quit");
+                count = Convert.ToInt32(Console.ReadLine()); // Converting count into an integer the user can input
+                Console.WriteLine();
+
+                if (count == 0) // if count is equal to 0, exit the app, always remember the BREAK!!!
+                {
+                    Console.WriteLine("Exiting the program");
+                    break; // REMEMBER THIS!!
+                }
+
+                sum += count; // this is the sum, addng each numbers
+                Console.WriteLine("Current total of all numbers added is  " + sum); // displaying result to the screen
+
+            }
+        }
+
+
+
+    }
+}
+
+
